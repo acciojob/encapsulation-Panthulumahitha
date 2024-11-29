6a94c82448e9b6f39bd27f6e8a918ffe71a32221
Panthulumahitha <panthulumahitha0620@gmail.com>
2024-11-29 13:51:09 +0530

    RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
-    
+    // Task 2: Private member variable
+    private String name;
+
+    // Task 5: Getter and Setter functions
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
